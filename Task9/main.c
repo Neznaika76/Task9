107f0b72b6f1395ec7c3cabaa57e828c006ffe32
Neznaika76 <n9032692530@gmail.com>
2024-01-25 13:11:07 +0300

    Задача 3

--- Task9/main.c
+++ Task9/main.c
@@ -8,6 +8,7 @@
 // Задание 1
 /*
 #include <stdio.h>
+#include <string.h>
  
 int main(int argc, const char * argv[])
 {
@@ -38,7 +39,7 @@ int main(int argc, const char * argv[])
 }
 */
 // Задание 2
-
+/*
 #include <stdio.h>
 #include <string.h>
 
@@ -67,5 +68,77 @@ int main(int argc, const char * argv[])
     fp = fopen("output.txt", "w");
     fprintf(fp, "%s", string);
     fclose(fp);
+    return 0;
+}
+*/
+// Задание 3
+
+#include <stdio.h>
+#include <string.h>
+
+
+int selectNums(char *str, int nums[])
+{
+    int i = 0, j = 0, num = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            while (str[i] >= '0' && str[i] <= '9')
+            {
+                num = num * 10 + (str[i] - 48);
+                ++i;
+            }
+            nums[j++] = num;
+            num = 0;
+        }
+        ++i;
+    }
+    return j;
+}
+    
+void sortAscend(int a[], int size)
+{
+     int i, j;
+    for (j = 1; j < size; ++j)
+        for (i = 0; i < size - j; ++i)
+            if (a[i] > a[i+1])
+            {
+                a[i]   = a[i] ^ a[i+1];
+                a[i+1] = a[i] ^ a[i+1];
+                a[i]   = a[i] ^ a[i+1];
+            }
+}
+
+int main(int argc, const char * argv[])
+{
+    FILE *f_in = fopen("input.txt", "r");
+    FILE *f_out = fopen("output.txt", "w");
+    int i, size;
+    int nums[500];
+    char str[1002];
+    char *estr;
+    
+    while (1)
+       {
+           estr = fgets(str, 1000, f_in);
+           if (estr == NULL)
+           {
+             if ( feof (f_in) != 0)
+                break;
+             else
+             {
+                 printf("Ошибка чтения из файла\n");
+                break;
+             }
+          }
+       }
+    
+    size = selectNums(str, nums);
+    sortAscend(nums, size);
+    for (i = 0; i < size; ++i)
+        fprintf(f_out, "%d ", nums[i]);
+    fclose(f_in);
+    fclose(f_out);
     return 0;
 }
